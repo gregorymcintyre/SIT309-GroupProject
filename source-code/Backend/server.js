42cca11469c02dc1fe66a5a94369545fe54057cc
Marshal Welgama <hmwelgam@deakin.edu.au>
2019-08-30 22:38:30 +1000

    UPDATE:Finished weather endpoint

--- source-code/Backend/server.js
+++ source-code/Backend/server.js
@@ -4,31 +4,37 @@ var request = require('request');
 const app = express()
 const port = 3000
 var weatherData = {}
+
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.get('/weather/current/:lat/:lng', function (req, res) {
-    request.get('https://api.darksky.net/forecast/2863177e8cb7681e2d21b3595b571989/42.3601,-71.0589', function(err, response, body) {
-        
-        const jsonString = JSON.stringify(req.body)
-    //console.log(jsonString)
+
+lat = req.params.lat
+lng = req.params.lng
+
+    request.get(`https://api.darksky.net/forecast/2863177e8cb7681e2d21b3595b571989/${lat},${lng}`, function(err, response, body) {
+
     weatherData = JSON.parse(body) //had to parse to a json, this part did my head in
-});
+    //Creating Json object
     var weather = {}
-    weather = 
+ 
+        //make weatherData json within the HTTP GET
+        weather = 
         {
             "latitude": weatherData.latitude,
             "longitude": weatherData.longitude,
             "timezone": weatherData.timezone,
-            "summary": weatherData.currently.summary
-           /* "temperature": weatherData.currently.tempreature,
+            "summary": weatherData.currently.summary,
+            "temperature": weatherData.currently.temperature,
             "apparentTemperature": weatherData.currently.apparentTemperature,
             "temperatureHigh": weatherData.currently.temperatureHigh,
             "temperatureLow": weatherData.currently.temperatureLow,
-            "windSpeed": weatherData.currently.windSpeed */
-        } 
-        //make weatherData json within the HTTP GET
-   res.send(weather)
-    
+            "windSpeed": weatherData.currently.windSpeed 
+        }
+        
+        res.send(weather)
+});
+
   })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
