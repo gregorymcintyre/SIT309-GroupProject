d3160e1d16fb4e0f911440f56babaffdc1838c12
Greg McIntyre <gregmcintyre1984@gmail.com>
2019-09-11 11:15:16 +1000

    UPDATE: Part 3b Complete, waiting for backend data

--- source-code/aws-iot-core-devices/GutterRash.js
+++ source-code/aws-iot-core-devices/GutterRash.js
@@ -6,7 +6,7 @@ const parking1 = awsIot.device({
 	certPath:	'./certificates/parking1/4c54dfc22e-certificate.pem.crt',
 	keyPath:	'./certificates/parking1/4c54dfc22e-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking2',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -14,7 +14,7 @@ const parking2 = awsIot.device({
 	certPath:	'./certificates/parking2/a6b4294fb4-certificate.pem.crt',
 	keyPath:	'./certificates/parking2/a6b4294fb4-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking2',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -22,7 +22,7 @@ const parking3 = awsIot.device({
 	certPath:	'./certificates/parking3/2554f696ef-certificate.pem.crt',
 	keyPath:	'./certificates/parking3/2554f696ef-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking3',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -30,7 +30,7 @@ const parking4 = awsIot.device({
 	certPath:	'./certificates/parking4/21e85a4a82-certificate.pem.crt',
 	keyPath:	'./certificates/parking4/21e85a4a82-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking4',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -38,7 +38,7 @@ const parking5 = awsIot.device({
 	certPath:	'./certificates/parking5/e1227c5cce-certificate.pem.crt',
 	keyPath:	'./certificates/parking5/e1227c5cce-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking5',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -46,7 +46,7 @@ const parking6 = awsIot.device({
 	certPath:	'./certificates/parking6/a450ae3190-certificate.pem.crt',
 	keyPath:	'./certificates/parking6/a450ae3190-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking6',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -54,7 +54,7 @@ const parking7 = awsIot.device({
 	certPath:	'./certificates/parking7/bbb6f3aded-certificate.pem.crt',
 	keyPath:	'./certificates/parking7/bbb6f3aded-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking7',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -62,21 +62,29 @@ const parking8 = awsIot.device({
 	certPath:	'./certificates/parking8/02a7125063-certificate.pem.crt',
 	keyPath:	'./certificates/parking8/02a7125063-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking8',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 const parking9 = awsIot.device({
 	certPath:	'./certificates/parking9/a7b98e4de2-certificate.pem.crt',
 	keyPath:	'./certificates/parking9/a7b98e4de2-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking9',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 const parking10 = awsIot.device({
 	certPath:	'./certificates/parking10/975dc8e754-certificate.pem.crt',
 	keyPath:	'./certificates/parking10/975dc8e754-private.pem.key',
 	caPath:		'./certificates/rootCA.crt',
-	clientId:	'gutterrash_1',
+	clientId:	'parking10',
+	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
+});
+
+const parking11 = awsIot.device({
+	certPath:	'./certificates/parking11/51e2e1c5c1-certificate.pem.crt',
+	keyPath:	'./certificates/parking11/51e2e1c5c1-private.pem.key',
+	caPath:		'./certificates/rootCA.crt',
+	clientId:	'parking11',
 	host:		'a3e9smmo06mnl8-ats.iot.us-east-1.amazonaws.com'
 });
 
@@ -85,81 +93,98 @@ const parking10 = awsIot.device({
 var connectCount = 0;	//to simplify connections in the future
 
 parking1.on('connect', function() {
-	isConnected=true;
+	isConnectedParking1=true;
 	connectCount++;
-	//console.log ('parking1');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking1 connected');
+	//console.log(connectCount + ' of 10 devices connected');
+		
+		//parking1.publish('parking1', '{"data" : "test"}');
+		//console.log ('parking1 message published');
 });
 
 parking2.on('connect', function() {
-	isConnected=true;
+	//parking2.on('error', (msg) => { console.log('error: ' + msg); });
+	
+	isConnectedparking2=true;
 	connectCount++;
-	//console.log ('parking2');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking2 connected');
+	//console.log(connectCount + ' of 10 devices connected');
+		
+		//parking2.publish('parking2', '{"data" : "test"}');
+		//console.log ('parking2 message published');
 });
 
 parking3.on('connect', function() {
-	isConnected=true;
+	isConnectedparking3=true;
 	connectCount++;
-	//console.log ('parking3');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking3 connected');
+	//console.log(connectCount + ' of 10 devices connected');
+	
+		//parking3.publish('parking3', '{"data" : "test"}');
+		//console.log ('parking3 message published');
+	
 });
 
 parking4.on('connect', function() {
-	isConnected=true;
+	isConnectedparking4=true;
 	connectCount++;
-	//console.log ('parking4');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking4 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking5.on('connect', function() {
-	isConnected=true;
+	isConnectedparking5=true;
 	connectCount++;
-	//console.log ('parking5');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking5 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking6.on('connect', function() {
-	isConnected=true;
+	isConnectedparking6=true;
 	connectCount++;
-	//console.log ('parking6');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking6 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking7.on('connect', function() {
-	isConnected=true;
+	isConnectedparking7=true;
 	connectCount++;
-	//console.log ('parking7');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking7 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking8.on('connect', function() {
-	isConnected=true;
+	isConnectedparking8=true;
 	connectCount++;
-	//console.log ('parking8');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking8 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking9.on('connect', function() {
-	isConnected=true;
+	isConnectedparking9=true;
 	connectCount++;
-	//console.log ('parking9');
-	console.log(connectCount + ' of 10 devices connected');
+	console.log ('parking9 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
 parking10.on('connect', function() {
-	isConnected=true;
+	isConnectedparking10=true;
 	connectCount++;
-	//console.log ('parking10');
-	console.log(connectCount + ' of 10 devices connected');
-});
-
-parking1.on('error', (msg) => {
-	console.log('error: ' + msg);
+	console.log ('parking10 connected');
+	//console.log(connectCount + ' of 10 devices connected');
 });
 
  //store is device is connected or not 
-let isConnected=false; 
+let isConnectedparking1=false; 
+let isConnectedparking2=false; 
+let isConnectedparking3=false; 
+let isConnectedparking4=false; 
+let isConnectedparking5=false; 
+let isConnectedparking6=false; 
+let isConnectedparking7=false; 
+let isConnectedparking8=false; 
+let isConnectedparking9=false; 
+let isConnectedparking10=false; 
 
 //store a reference carSimulation method callback 
 let timeout=null; 
@@ -204,44 +229,75 @@ let timeout=null;
 		
 		console.log('Publish initiated');
 		
-		//parking1.publish('',
-			//'{
-			//"bayID": "1005",
-			//"status": "Unoccupied",
-			//"restriction_duration": 30
-			//}'
-		//	'parking1 test'
-		//)
+		try{
+			parking1.publish('parking1', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking1 message published');
+		}catch(err){
+			console.log(err.name);
+		}
+
+		try{
+			parking2.publish('parking2', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking2 message published');
+		}catch(err){
+			console.log(err.name);
+		}
+
+		try{
+			parking3.publish('parking3', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking3 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking1.publish('', 'Inital Message');
-		console.log ('parking1 message published');
+		try{
+			parking4.publish('parking4', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking4 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking2.publish('', 'Inital Message');
-		console.log ('parking2 message published');
+		try{
+			parking5.publish('parking5', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking5 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking3.publish('', 'Inital Message');
-		console.log ('parking3 message published');
+		try{
+			parking6.publish('parking6', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking6 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking4.publish('', 'Inital Message');
-		console.log ('parking4 message published');
+		try{
+			parking7.publish('parking7', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking7 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking5.publish('', 'Inital Message');
-		console.log ('parking5 message published');
+		try{
+			parking8.publish('parking8', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking8 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking6.publish('', 'Inital Message');
-		console.log ('parking6 message published');
+		try{
+			parking9.publish('parking9', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking3 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
-		parking7.publish('', 'Inital Message');
-		console.log ('parking7 message published');
-		
-		parking8.publish('', 'Inital Message');
-		console.log ('parking8 message published');
-		
-		parking9.publish('', 'Inital Message');
-		console.log ('parking9 message published');
-		
-		parking10.publish('', 'Inital Message');
-		console.log ('parking10 message published');
+		try{
+			parking10.publish('parking10', '{"bayID": "1005", "status": "Unoccupied", "restriction_duration": 30 }');
+			console.log ('parking3 message published');
+		}catch(err){
+			console.log(err.name);
+		}
 		
 	}
 
@@ -264,16 +320,18 @@ let timeout=null;
 		
 	}
 	
+	
 	setInterval(
 		function(){
-			console.log('timer test 1min');
-			if (connectCount == 10){
-				queryBays(101, 102, 103, 104, 105, 106, 107, 108, 109, 110);
+			//console.log('timer test');
+			//if (connectCount >= 3){
+				//queryBays(101, 102, 103, 104, 105, 106, 107, 108, 109, 110);
 				publishData();
 				//connectCount = 0;
-			}
-		}, 60000
+			//}
+		}, 10000		//10 seconds
 	);
+	
 	
 	
 	
